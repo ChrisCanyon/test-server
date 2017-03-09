130f44cd45a1d00ae9dae9dd9c9e1a0b55ac3f2e
Chris Nagy <chrisnagy@Chriss-MacBook.local>
2017-03-09 15:37:59 -0500

    cole's a bitch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def letsgo
-  	render html: "fuck me"
+  	render html: "Cole's a Bitch.\n\nThat is all."
   end
 
 end
