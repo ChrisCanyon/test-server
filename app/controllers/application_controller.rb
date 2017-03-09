295d8301b4a5284d68b26a141bbecfdfc5f1b368
Chris Nagy <chrisnagy@Chriss-MacBook.local>
2017-03-09 15:42:13 -0500

    cole's a bitch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def letsgo
-  	render html: "Cole's a Bitch.\n\nThat is all."
+  	render html: "Cole's a Bitch."
   end
 
 end
